c879c54ae15bbb59550e2280336baee70d6ea84c
AlexisNMunoz <alexisnahuelmunioz@gmail.com>
2024-06-20 17:34:26 -0300

    fixed: pt-10

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,7 +2,7 @@
 export default function AboutMe() {
     return (
         <section className="h-screen flex justify-center items-center">
-            <div className="wrapper grid grid-cols-1 md:grid-cols-2 items-center">
+            <div className="wrapper pt-10 grid grid-cols-1 md:grid-cols-2 items-center">
                 <div className="space-y-4 bg-white  p-2 shadow-md rounded-md md:order-1">
                     <h2 className="text-4xl text-center md:text-left text-slate-800">Tu frase aqui</h2>
                     <p className="text-slate-500">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti dignissimos quibusdam nobis!</p>
