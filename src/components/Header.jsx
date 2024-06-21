3ea3620cf8b56ecdb354080dcf3c7cd26df3d774
AlexisNMunoz <alexisnahuelmunioz@gmail.com>
2024-06-21 15:06:00 -0300

    add: sobre mi in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,7 @@ export default function Header() {
                 <div className="fixed inset-0 bg-gradient-to-b from-white/70 to-black/70 translate-x-full peer-checked:translate-x-0 md:static md:translate-x-0 md:bg-none">
                     <ul className="bg-white absolute items-center inset-x-0 w-[90%] mx-auto top-24 p-12 rounded-md shadow-lg text-center grid justify-items-center gap-6 md:static md:w-max md:bg-transparent md:shadow-none md:p-0 md:grid-flow-col">
                         <li><a className="hover:text-violet-700 transition-colors" href="#">Inicio</a></li>
+                        <li><a className="hover:text-violet-700 transition-colors" href="#">Sobre mi</a></li>
                         <li><a className="hover:text-violet-700 transition-colors" href="#">Proyectos</a></li>
                         <li><a className="hover:text-violet-700 transition-colors" href="#">Skills</a></li>
                         <div className="gap-4 basis-0 justify-end items-center flex md:hidden">
